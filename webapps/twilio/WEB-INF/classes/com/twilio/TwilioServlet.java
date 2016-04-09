3c614a34b0028c15d8d0da75fac7bd5f051fd8c6
stevenroach7 <sroach@macalester.edu>
2016-04-09 04:44:53 -0500

    Added history array to service response method.

--- webapps/twilio/WEB-INF/classes/com/twilio/TwilioServlet.java
+++ webapps/twilio/WEB-INF/classes/com/twilio/TwilioServlet.java
@@ -3,41 +3,40 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.HashMap;
- 
+
 import com.twilio.sdk.verbs.TwiMLResponse;
 import com.twilio.sdk.verbs.TwiMLException;
 import com.twilio.sdk.verbs.Message;
- 
+
 public class TwilioServlet extends HttpServlet {
- 
+
+
+
+
+    // service() responds to both GET and POST requests.
+    // You can also use doGet() or doPost()
     public void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        // Create a dict of people we know.
-        HashMap<String, String> callers = new HashMap<String, String>();
-        callers.put("+16124064267", "Eloa");
-        callers.put("+16512708200", "Steven");
-        callers.put("+12159001202", "Kai");
- 
-        String fromNumber = request.getParameter("From");
-        String knownCaller = callers.get(fromNumber);
-        String message;
-        if (knownCaller == null) {
-            // Use a generic message
-            message = "Monkey, thanks for the message!";
-        } else {
-            // Use the caller's name
-            message = knownCaller + ", thanks for the message!";
-        }
- 
+
+        HttpSession session = request.getSession(true);
+
+        ArrayList<int> history = session.getAttribute("history");
+        if history == null) {
+          session.setAttribute("history", new ArrayList<int>());
+          history = session.getAttribute("history");
+        } 
+        history.add(1);
+
+        String message = "You have texted " + history.size() + " times.";
+
         // Create a TwiML response and add our friendly message.
         TwiMLResponse twiml = new TwiMLResponse();
-        Message sms = new Message(message);
+        Sms sms = new Sms(message);
         try {
             twiml.append(sms);
         } catch (TwiMLException e) {
             e.printStackTrace();
         }
- 
+
         response.setContentType("application/xml");
         response.getWriter().print(twiml.toXML());
     }
