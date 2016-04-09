b688744410e198ba12f8b80b06639b69b2b0b04c
Eloá <efrancav@macalester.edu>
2016-04-09 15:59:44 -0500

    player updated

--- webapps/twilio/WEB-INF/classes/com/twilio/Player.java
+++ webapps/twilio/WEB-INF/classes/com/twilio/Player.java
@@ -1,43 +1,50 @@
 package com.twilio;
 
+import java.util.ArrayList;
+
 /**
  * Created by Brett Graham on 4/9/2016.
  */
 public class Player {
 
     private String name;
-    private int maxHealth = 10;
-    public int health = maxHealth;
-    public boolean hasCrowbar;
+    private ArrayList<String> objects;
+    private ArrayList<Integer> history;
 
-        /**
+    /**
         * A constructor for the player class, it takes in one parameter, the name of the player.
         *
-        * @param name The name of the player
+        *
         */
-         public Player(String name) {
-          this.name = name;
-        }
-
-        //method to work with damaging or healing the player
-        public boolean changeHealth(int change) {
-            if (health + change <= 0) {
-                return false; //in case the player dies.
-            } else if (health + change > maxHealth) { //if this action would put the player above max health
-                health = maxHealth;
-                return true;
-            } else {
-                health += change; //the players health is now equal to the previous health plus the change. Negative values are allowed.
-                return true;
-            }
+         public Player() {
+             this.name = name;
+             objects = new ArrayList<String>();
+             history = new ArrayList<Integer>();
+             history.add(0);
         }
 
 
         //just a method to update the status of multiple items on the player.
-        public void findItem(String item) {
-            if (item.equals("Crowbar")) {
-                hasCrowbar = true;
-            }
+        public boolean findItem(String item) {
+           for(String object: objects){
+               if(item.equals(object)) return true;
+           }
+            return false;
         }
+
+        public void addItem(String item){
+            objects.add(item);
+        }
+
+        public void addToHistory(Integer currentPosition){
+            history.add(currentPosition);
+        }
+
+        public Integer getCurrentPosition(){
+            if(history.size()<= 1) return history.get(0);
+            return history.get(history.size()-1);
+        }
+
+
  }
 
