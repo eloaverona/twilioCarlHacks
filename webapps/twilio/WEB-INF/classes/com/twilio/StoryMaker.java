f8233a3a97583a592c91b85add3f92ccecbf0472
stevenroach7 <sroach@macalester.edu>
2016-04-09 15:11:33 -0500

    Simplified the code slightly in StoryMaker

--- webapps/twilio/WEB-INF/classes/com/twilio/StoryMaker.java
+++ webapps/twilio/WEB-INF/classes/com/twilio/StoryMaker.java
@@ -16,8 +16,8 @@ public class StoryMaker {
 
 	public StoryMaker(ArrayList<Integer> history) {
         this.history = history;
-	      TrialNodeTest testNodes = new TrialNodeTest();
-	      this.mapFromIntToNode = testNodes.getMapFromIntToNode();
+		TrialNodeTest testNodes = new TrialNodeTest();
+		this.mapFromIntToNode = testNodes.getMapFromIntToNode();
     }
 
 
@@ -26,27 +26,27 @@ public class StoryMaker {
     public String makeResponse(String message) {
 
 
+
 		Node currentNode = getCurrentNode();
-		String defaultResponse = "You have entered an invalid command. Please try again. ";
-		String response = defaultResponse; //default response is the invalid user input response.
+		String response = "You have entered an invalid command. Please try again. "; //default response is the invalid user input response.
 
-    	  if (history.size() == 0) {
-    		    response = currentNode.getText();
-    		    history.add(currentNode.getIdentifier());
-    		    return response;
-    	  }
+		if (history.size() == 0) {
+			response = currentNode.getText();
+			history.add(currentNode.getIdentifier());
+			return response;
+		}
 
-    	  if (message == null) return response;
+		if (message == null) return response;
 
-    	  Node nextNode = currentNode.whatNext(message); // Add Failsafe to tell user if they have an invalid command
+		Node nextNode = currentNode.whatNext(message); // Add FailSafe to tell user if they have an invalid command
 
-    	  if(nextNode == null) { // This might be the end of the game or a dead end.
-    		  response = defaultResponse + currentNode.getText(); // Give error message + Text of last node.
-    		  return response;
-    	  }
+		if(nextNode == null) { // This might be the end of the game or a dead end.
+			response += currentNode.getText(); // Give error message + Text of last node.
+			return response;
+		}
         response = nextNode.getText();
-    	  history.add(nextNode.getIdentifier());
-    	  if (response == null) response = "no text for this node. There is a problem with the graph data structure."; // If there is a mistake in the program
+		history.add(nextNode.getIdentifier());
+		if (response == null) response = "no text for this node. There is a problem with the directed graph data structure. Congrats, you found a bug."; // If there is a mistake in the program
 
         return response;
     }
