1c89d7ff63aa36b128d2882a8901ba4d4e188f6b
Eloá <efrancav@macalester.edu>
2016-04-09 21:11:00 -0500

    get and use key working

--- webapps/twilio/WEB-INF/classes/com/twilio/TrialNodeTest.java
+++ webapps/twilio/WEB-INF/classes/com/twilio/TrialNodeTest.java
@@ -26,10 +26,11 @@ public class TrialNodeTest {
         Node node1 = new Node(0, "Welcome to the game. Text anything to continue");
         Node node2 = new Node(2, "You open your eyes and find yourself in a dimly lit room, immediately you try and stand up from the chair you find yourself in but something restrains you. Your wrists and ankles have been tied to the chair with rope, after a brief struggle you manage to free yourself. A quick glance of the room teaches you a few things. There seems to be a wooden door to the right, along with a desk directly ahead of you. The rest of room is remarkably empty, save for the ubiquitous cobwebs of course. a: Check out the door, b: Check out the desk.");
         Node node21 = new Node(21, "The door is made up of clearly old wood, you would think it would break easily but the lock seems sturdy and thick. a: Try and open the door, b: Go back.");
-        Node node22 = new Node(22, "The desk is made up of hard redwood, but you almost couldn’t tell given all the papers scattered across the top. Along with the papers you notice a drawer. a: Investigate the paper, b: Open the drawer");
+        Node node22 = new Node(22, "The desk is made up of hard redwood, but you almost couldn’t tell given all the papers scattered across the top. Along with the papers you notice a drawer. a: Investigate the paper, b: Open the drawer, c: go back");
        // Node node211 = new Node(211, "MUST CLEAR UP HOW TO HANDLE KEY SCENERIO. a: Try and open the door, b: Go back");
         Node node221 = new Node(221, "You look over a few of the papers, all of them are dated in the 1970’s and seem to concern some company named “Odin”. After a while you get bored and decide to stop reading. a: go back");
         Node node222 = new Node(222, "The drawer is empty and remarkably clean when compared to the rest of the room. Perfectly placed in the center is a single brass key. You take it and shut the drawer.. a: go back");
+        node222.setObjectFound("key");
         Node node3 = new Node(3, "The brass key seems to perfect fit the lock of this door. You try it and sure enough the door creaks open. You moved through the door into the next room. You lose the Brass key.\n" +
                 "This room is just as dark as the last, from the low lying ceiling hangs a single lantern with a flame that seems on its last breath. It’s hard to make out from here but there seems to be a stuffed animal on a table stand on the other side of the room. You also notice a dirty mirror to your left.");
         node3.setObjectNeeded("key");
@@ -45,6 +46,7 @@ public class TrialNodeTest {
 
         optionsNode22.put("a", node221);
         optionsNode22.put("b", node222);
+        optionsNode22.put("c", node2);
 
 
         optionsNode211.put("a", node21);
