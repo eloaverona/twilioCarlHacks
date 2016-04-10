181e1f0164e5345b8b8cb68200c10c5add5cb094
stevenroach7 <sroach@macalester.edu>
2016-04-10 00:53:56 -0500

    Made some changes to clean up the StoryMaker class.

--- webapps/twilio/WEB-INF/classes/com/twilio/StoryMaker.java
+++ webapps/twilio/WEB-INF/classes/com/twilio/StoryMaker.java
@@ -1,6 +1,5 @@
 package com.twilio;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 
 
@@ -27,15 +26,17 @@ public class StoryMaker {
 
     public String makeResponse(String message) {
 
-		String response = "You entered an invalid command. Please try again. "; //default response is the invalid user input response.
-		if(player.historySize() == 0){
-			//message = "a";
+		String response = "You have entered an invalid command. Please try again. "; // default response is the invalid user input response.
+		if(player.historySize() == 0){ // If player has just begun game.
 
 			player.addToHistory(0);
-			response = "Hello and welcome. This is a text adventure game where you will be given some descriptive text from which you can decide how you want to respond. There may be some areas or actions that you may not be able to complete at certain points in the story but may be able complete them later on. Be warned that as a player you death is possible in this game. There may also be times when your actions are not immediate. Your head constantly in a haze, with lapses coming and going. That being said, have fun and let the game begin! Reply yeah! to start!";
+			response = "Hello and welcome. This is a text adventure game where you will be given some descriptive text from which you can decide how you want to respond. " +
+					"There may be some areas or actions that you may not be able to complete at certain points in the story, but you may be able to complete them later on. " +
+					"There may also be times when your actions are not immediate. With your head constantly in a haze, lapses come and go. " +
+					"That being said, have fun and let the game begin! Reply yeah! to start!";
 			return response;
 		}
-		if(player.historySize() == 1){
+		if (player.historySize() == 1) { // Player is now ready to start game.
 			message = "a";
 		}
 		Node currentNode = getCurrentNode();
@@ -46,29 +47,22 @@ public class StoryMaker {
 		Node nextNode = currentNode.whatNext(message); // Add FailSafe to tell user if they have an invalid command
 
 
-		if(nextNode == null) {  // If user didn't enter a valid command
+		if (nextNode == null) {  // If user didn't enter a valid command
 			return response + currentNode.getText(); // Give error message + Text of last node.
 		}
 
-		// Return the node the player tried to go to. Storymaker asks what do you need.Node says I need the key. Check player for the key. If player has key,
+		// Return the node the player tried to go to. Storymaker asks what do you need. Node says I need the key. Check player for the key. If player has key,
 		// Allow player to go to that node. else make player go back to currentNode.
-		// TODO: Add a new method to determine where they go next and get the message to send.
-
 		String altMessage = "";
 		if (nextNode.getObjectNeeded() != null ) { // If node needs an object
-			if (player.getObjects().contains(nextNode.getObjectNeeded())) { // If player has object
-				//player.dropItem(nextNode.getObjectNeeded());
-
-			} else { // If player doesn't have object
+			if (!player.getObjects().contains(nextNode.getObjectNeeded())) { // If player doesn't have object needed.
 				nextNode = mapFromIntToNode.get(nextNode.getAltNodeID());
 				altMessage = nextNode.getText() + " ";
 				nextNode = currentNode;
+
 			}
 		}
 
-
-
-		// This is allowedNode instead of nextNode. The node the player actually is going to now.
         response = nextNode.getText();
 		player.addToHistory(nextNode.getIdentifier());
 
@@ -78,12 +72,14 @@ public class StoryMaker {
 			player.clearHistory();
 		}
 
+		if (nextNode.getIdentifier() == 9) { // Drop the knife if player reaches node 9.
+			player.dropItem("knife");
+		}
 
-
-		if (nextNode.getObjectFound() != null) {
+		if (nextNode.getObjectFound() != null) { // If player finds an object at this node
 			player.addItem(nextNode.getObjectFound());
 		}
-		if (response == null) response = "no text for this node. There is a problem with the directed graph data structure. Congrats, you found a bug."; // If there is a mistake in the program
+		if (response == null) response = "You have stumbled upon the unknown, an endless stream of dark matter. AKA Congrats, you found a bug."; // If there is a mistake in the program
         return altMessage + response;
     }
 
